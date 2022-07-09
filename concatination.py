9ff93511c8e28f1bf1a4759aa1ca846fa83d2fd8
Chaitanya Bhilange <107292155+ChaitanyaBhilange@users.noreply.github.com>
2022-07-08 23:20:42 -0700

    Add files via upload

---
+++ concatination.py
@@ -0,0 +1,23 @@
+    #wpp to peform concatination and repatiton  on strings
+
+#Concatinationf
+
+f_name='Chaitanya'
+m_name='Panjabrao'
+l_name='Bhilange'
+
+
+#Method1
+print(f_name,m_name,l_name)
+
+#Method2
+print(f_name+' '+m_name+' '+l_name)
+
+#Method3
+print(f'hello {f_name}')
+
+#Method4
+print("hi {} {},how are you?".format(f_name,l_name))
+
+#Repetation
+print(f_name*4)      
